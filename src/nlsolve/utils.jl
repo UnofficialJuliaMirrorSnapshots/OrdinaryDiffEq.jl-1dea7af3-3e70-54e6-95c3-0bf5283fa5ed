33d800f2ae196cdcc8be1e8add69f828ce66542b
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-05-01 02:43:39 +0000

    Snapshot of branch master taken on 2019-04-30T22:43:37.543-04:00 from "https://github.com/UnofficialJuliaMirror/OrdinaryDiffEq.jl-1dea7af3-3e70-54e6-95c3-0bf5283fa5ed"

--- src/nlsolve/utils.jl
+++ src/nlsolve/utils.jl
@@ -117,7 +117,7 @@ DiffEqBase.@def iipnlsolve begin
         W = WOperator(f, dt, true)
         J = nothing # is J = W.J better?
       else
-        J = fill(zero(uEltypeNoUnits),length(u),length(u)) # uEltype?
+        J = false .* vec(u) .* vec(u)'
         W = similar(J)
       end
     end
